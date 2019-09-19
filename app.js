dba4b6c5e55e416a455cc9430a2c38e7924e8929
Muhammad Nur Ali <muhammadnurali303@gmail.com>
2019-09-19 11:43:28 +0700

    changed mongo connection

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 // db
 mongoose
-    .connect(process.env.MONGO_LOCAL, {
+    .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true
     })
